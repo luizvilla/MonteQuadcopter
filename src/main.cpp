d8f88254391c6c1b22dff65a9930cfd248524b5a
Luiz-Fernando Lavado-Villa <lflavado@tu-te-calmes>
2021-06-02 16:14:18 +0200

    New more readable code

--- src/main.cpp
+++ src/main.cpp
@@ -4,168 +4,148 @@
 // PROGRAMMER - Luiz Villa
 //---------------------------------------
 
-
-#include "Arduino.h"
-// Initial includes
-#include <SoftwareSerial.h>
-SoftwareSerial mySerial (7, 8); //RX, TX
-#include "I2Cdev.h"
-#include "MPU6050_6Axis_MotionApps20.h"
-#include <PID_v1.h>
-#include <EEPROM.h>
-//#include "MPU6050.h" // not necessary if using MotionApps include file
-
-
-#define ADDR_PITCH_OFFSET 0
-#define ADDR_ROLL_OFFSET 1
-#define FL_MOTOR 3
-#define FR_MOTOR 9
-#define BR_MOTOR 10
-#define BL_MOTOR 11
-
-
-//////////////////////////////////////////////
-//        RemoteXY include library          //
-//////////////////////////////////////////////
-
-// RemoteXY select connection mode and include library 
-//#define REMOTEXY__DEBUGLOGS Serial
-
-// // RemoteXY select connection mode and include library 
-#define REMOTEXY_MODE__BLE_NANO
-
-#include "RemoteXY.h"
-
-// RemoteXY connection settings 
-#define REMOTEXY_SERIAL Serial
-#define REMOTEXY_SERIAL_SPEED 9600
-
-
-// ----------------------------------------------------------------------------
-
-// RemoteXY configurate  
-#pragma pack(push, 1)
-uint8_t RemoteXY_CONF[] =
-  { 255,0,0,4,0,90,0,11,13,0,
-  129,0,47,14,12,2,17,77,111,116,
-  111,114,32,83,112,101,101,100,0,66,
-  130,47,18,13,10,2,26,66,130,67,
-  18,13,10,2,26,129,0,71,14,5,
-  2,17,80,105,116,99,104,0,66,130,
-  47,36,13,10,2,26,129,0,51,32,
-  5,2,17,82,111,108,108,0,66,130,
-  67,36,13,10,2,26,129,0,72,32,
-  4,2,17,89,97,119,0 };
-  
-// this structure defines all the variables and events of your control interface 
-struct {
-
-    // output variables
-  int8_t motor_speed; // =0..100 level position 
-  int8_t show_pitch; // =0..100 level position 
-  int8_t show_roll; // =0..100 level position 
-  int8_t show_yaw; // =0..100 level position 
-
-    // other variable
-  uint8_t connect_flag;  // =1 if wire connected, else =0 
-
-} RemoteXY;
-#pragma pack(pop)
-
-
-/////////////////////////////////////////////
-//           END RemoteXY include          //
-///////////////////////////////////////////// 
-
-
-
-//---------------------------------PID------------------------------------
-//Define Variables we'll be connecting to
-double rollSetpoint, rollInput, rollOutput;
-double pitchSetpoint, pitchInput, pitchOutput;
-double yawSetpoint, yawInput, yawOutput;
-
-//Define the aggressive and conservative Tuning Parameters
-double consKp = 0.5, consKi = 0.05, consKd = 0.05;
-
-PID pitchPID(&rollInput, &rollOutput, &rollSetpoint, consKp, consKi, consKd, DIRECT);
-PID rollPID(&pitchInput, &pitchOutput, &pitchSetpoint, consKp, consKi, consKd, DIRECT);
-//------------------------------------------------------------------------
-
-
-// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
-// is used in I2Cdev.h
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-#include "Wire.h"
-#endif
-
-// class default I2C address is 0x68
-// specific I2C addresses may be passed as a parameter here
-// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
-// AD0 high = 0x69
-MPU6050 mpu;
-//MPU6050 mpu(0x69); // <-- use for AD0 high
-
-#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
-bool blinkState = false;
-
-// MPU control/status vars
-bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
-
-// orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float euler[3];         // [psi, theta, phi]    Euler angle container
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-float ypr_cal[3]= {-0.55, -0.03, 0.01}; // initilizes the calibration (done by hand. This should be automated)
-
-// ================================================================
-// ===               INTERRUPT DETECTION ROUTINE                ===
-// ================================================================
-
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
-  mpuInterrupt = true;
-}
-
-
-#define ALPHA 0.1
-#define MULTIPLIER 6.67
-float motorBattery;
-
-int targetSpeed[4];
-
-float batt_level = 0;
+#include "quad_init.h"  // Header that includes all that is needed to start the quadCopter
 
 // ================================================================
 // ===                      INITIAL SETUP                       ===
 // ================================================================
 void setup() {
-  RemoteXY_Init ();  
+  //RemoteXY_Init ();  
 
-  // Serial.begin(9600);
-  // Serial.println("INITIALIZING");
+  quadInit(); // Function to initialize the quadCopter
+}
+
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
+int myReading = 0;
+int pwm_out = 0;
+int delay_count = 0; //count the number of delays - used to increment the motor speed slowly
+
+void loop() {
+
+  //RemoteXY_Handler (); 
+
+  // reset interrupt flag and get INT_STATUS byte
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
+
+  // get current FIFO count
+  fifoCount = mpu.getFIFOCount();
+
+  // check for overflow (this should never happen unless our code is too inefficient)
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+    // reset so we can continue cleanly
+    mpu.resetFIFO();
+    //        mySerial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+  } else if (mpuIntStatus & 0x02) {
+    // wait for correct available data length, should be a VERY short wait
+    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+    // read a packet from FIFO
+    mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+    // track FIFO count here in case there is > 1 packet available
+    // (this lets us immediately read more without waiting for an interrupt)
+    fifoCount -= packetSize;
+
+
+    // display Euler angles in degrees
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+
+    //compensates for the offset and converts in degrees
+    yawInput   = (ypr[0]-ypr_cal[0]) * 180 / M_PI;  //compensates for the offset and converts in degrees
+    pitchInput = (ypr[1]-ypr_cal[1]) * 180 / M_PI;
+    rollInput  = (ypr[2]-ypr_cal[2]) * 180 / M_PI;
+
+    batt_level = analogRead(A0) / 1023.0 * MULTIPLIER;
+
+    RemoteXY.motor_speed = pwm_out;
+    RemoteXY.show_pitch = pitchInput;
+    RemoteXY.show_roll = rollInput;
+    RemoteXY.show_yaw = yawInput;
+
+
+      // pwm_out = 5;
+      setSpeed(pwm_out);
+      // runIndividual(targetSpeed);
+      // Serial.print(F("speed="));
+      // Serial.println(pwm_out);
+      if(delay_count==2000) pwm_out++;
+      if(pwm_out > 50) pwm_out = 1 ;
+  }
+//  delay(100);
+  delay_count++;
+  if(delay_count > 2000) delay_count = 1 ;
+}
+
+
+// ---------------- OTHER FUNCTIONS --------------- 
+
+float smoothBattery (float prevEntry, float newEntry, float alpha) {
+  return (1-alpha) * prevEntry + alpha * newEntry;
+}
+
+void setSpeed(int val) {
+  analogWrite(FL_MOTOR, val);
+  analogWrite(FR_MOTOR, val);
+  analogWrite(BR_MOTOR, val);
+  analogWrite(BL_MOTOR, val);
+}
+
+void stabilise (int* currSpeed, int* actSpeed, float rollDiff, float pitchDiff) {
+  actSpeed[0] = (int) currSpeed[0] + (rollDiff) - (pitchDiff);  //each motor has actual Speed and speed at which we want them to fly...
+  actSpeed[1] = (int) currSpeed[1] + (rollDiff) + (pitchDiff);
+  actSpeed[2] = (int) currSpeed[2] - (rollDiff) + (pitchDiff);  //actual Speed is calculated as follows +- half rollDiff +- half pitchDiff
+  actSpeed[3] = (int) currSpeed[3] - (rollDiff) - (pitchDiff);
+
+  for (int i = 0; i < 4; i ++) {
+    if (actSpeed[i] < 0 )
+      actSpeed[i] = 0;
+  }
+}
+
+void checkIndividual (int motor, int* actSpeed) {
+  analogWrite(FL_MOTOR, 0);
+  analogWrite(FR_MOTOR, 0);
+  analogWrite(BR_MOTOR, 0);
+  analogWrite(BL_MOTOR, 0);
+
+  if (motor == 0)
+    analogWrite(FL_MOTOR, actSpeed[0]);
+  if (motor == 1)
+    analogWrite(FR_MOTOR, actSpeed[1]);
+  if (motor == 2)
+    analogWrite(BR_MOTOR, actSpeed[2]);
+  if (motor == 3)
+    analogWrite(BL_MOTOR, actSpeed[3]);
+}
+
+void runIndividual (int* actSpeed) {
+  analogWrite(FL_MOTOR, actSpeed[0]);
+  analogWrite(FR_MOTOR, actSpeed[1]);
+  analogWrite(BR_MOTOR, actSpeed[2]);
+  analogWrite(BL_MOTOR, actSpeed[3]);
+}
+
+void quadInit(){
+Serial.begin(9600);
+  Serial.println("INITIALIZING");
   
-  // for (int i = 0; i < 3; i++) {
-  //   ypr_cal[i] = 0.0;
-  // }
 
   //Enable internal reference of 1.1V
   //initialise battery level array with current battery level value
   pinMode(A0, INPUT);
   analogReference(INTERNAL);
   batt_level = analogRead(A0) / 1023.0 * MULTIPLIER;
-  motorBattery = 4.2;
-  // Serial.print("Battery level");
-  // Serial.println(batt_level);
+
+  motorBattery = 3.7;
+  Serial.print("Battery level");
+  Serial.println(batt_level);
 
   //------------------------------PID----------------------------------
   //initialize the variables we're linked to
@@ -264,94 +244,12 @@ void setup() {
   // configure LED for output
   pinMode(LED_PIN, OUTPUT);
 
+
 }
 
-// ================================================================
-// ===                    MAIN PROGRAM LOOP                     ===
-// ================================================================
-int myReading = 0;
-int pwm_out = 0;
-int delay_count = 0; //count the number of delays - used to increment the motor speed slowly
-
-void loop() {
-
-  RemoteXY_Handler (); 
 
 
-  //RemoteXY.pwm_out = 35;
-
-
-  // RemoteXY.roll = 30;
-  // RemoteXY.yaw = 15;
-
-  // RemoteXY.pwm_out = pwm_out>>3;
-
-  // if programming failed, don't try to do anything
-  // if (!dmpReady) return;
-
-  // if (mySerial.available()) {
-  //   myReading = mySerial.parseInt();
-  //   for (int i = 0; i < 4; i++) {
-  //     targetSpeed[i] = myReading;
-  //   }
-
-  //   runIndividual(targetSpeed);
-  //   while (mySerial.available())  //flushing anything that wasn't read
-  //     mySerial.read();
-  //        runIndividual(myReading);
-  //        checkMotor(myReading);
-  //          checkIndividual(myReading);
-  // }
-
-  // wait for MPU interrupt or extra packet(s) available
-  //    while (!mpuInterrupt && fifoCount < packetSize) {
-  // // if you are really paranoid you can frequently test in between other
-  // // stuff to see if mpuInterrupt is true, and if so, "break;" from the
-  // // while() loop to immediately process the MPU data
-  //    }
-
-  // reset interrupt flag and get INT_STATUS byte
-  mpuInterrupt = false;
-  mpuIntStatus = mpu.getIntStatus();
-
-  // get current FIFO count
-  fifoCount = mpu.getFIFOCount();
-
-  // check for overflow (this should never happen unless our code is too inefficient)
-  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
-    // reset so we can continue cleanly
-    mpu.resetFIFO();
-    //        mySerial.println(F("FIFO overflow!"));
-
-    // otherwise, check for DMP data ready interrupt (this should happen frequently)
-  } else if (mpuIntStatus & 0x02) {
-    // wait for correct available data length, should be a VERY short wait
-    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-
-    // read a packet from FIFO
-    mpu.getFIFOBytes(fifoBuffer, packetSize);
-
-    // track FIFO count here in case there is > 1 packet available
-    // (this lets us immediately read more without waiting for an interrupt)
-    fifoCount -= packetSize;
-
-
-//     // display Euler angles in degrees
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    mpu.dmpGetGravity(&gravity, &q);
-    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-
-    //compensates for the offset and converts in degrees
-    yawInput   = (ypr[0]-ypr_cal[0]) * 180 / M_PI;  //compensates for the offset and converts in degrees
-    pitchInput = (ypr[1]-ypr_cal[1]) * 180 / M_PI;
-    rollInput  = (ypr[2]-ypr_cal[2]) * 180 / M_PI;
-
-    batt_level = analogRead(A0) / 1023.0 * MULTIPLIER;
-
-    RemoteXY.motor_speed = pwm_out;
-    RemoteXY.show_pitch = pitchInput;
-    RemoteXY.show_roll = rollInput;
-    RemoteXY.show_yaw = yawInput;
+//UNUSED CODE
 
     // yawInput   = ypr[0];  //compensates for the offset and converts in degrees
     // pitchInput = ypr[1];
@@ -454,62 +352,3 @@ void loop() {
   //   // blink LED to indicate activity
   //   blinkState = 1;
   //   digitalWrite(LED_PIN, blinkState);
-
-      // pwm_out = 5;
-      setSpeed(pwm_out);
-      // runIndividual(targetSpeed);
-      // Serial.print(F("speed="));
-      // Serial.println(pwm_out);
-      if(delay_count==2000) pwm_out++;
-      if(pwm_out > 50) pwm_out = 1 ;
-  }
-//  delay(100);
-  delay_count++;
-  if(delay_count > 2000) delay_count = 1 ;
-}
-
-float smoothBattery (float prevEntry, float newEntry, float alpha) {
-  return (1-alpha) * prevEntry + alpha * newEntry;
-}
-
-void setSpeed(int val) {
-  analogWrite(FL_MOTOR, val);
-  analogWrite(FR_MOTOR, val);
-  analogWrite(BR_MOTOR, val);
-  analogWrite(BL_MOTOR, val);
-}
-
-void stabilise (int* currSpeed, int* actSpeed, float rollDiff, float pitchDiff) {
-  actSpeed[0] = (int) currSpeed[0] + (rollDiff) - (pitchDiff);  //each motor has actual Speed and speed at which we want them to fly...
-  actSpeed[1] = (int) currSpeed[1] + (rollDiff) + (pitchDiff);
-  actSpeed[2] = (int) currSpeed[2] - (rollDiff) + (pitchDiff);  //actual Speed is calculated as follows +- half rollDiff +- half pitchDiff
-  actSpeed[3] = (int) currSpeed[3] - (rollDiff) - (pitchDiff);
-
-  for (int i = 0; i < 4; i ++) {
-    if (actSpeed[i] < 0 )
-      actSpeed[i] = 0;
-  }
-}
-
-void checkIndividual (int motor, int* actSpeed) {
-  analogWrite(FL_MOTOR, 0);
-  analogWrite(FR_MOTOR, 0);
-  analogWrite(BR_MOTOR, 0);
-  analogWrite(BL_MOTOR, 0);
-
-  if (motor == 0)
-    analogWrite(FL_MOTOR, actSpeed[0]);
-  if (motor == 1)
-    analogWrite(FR_MOTOR, actSpeed[1]);
-  if (motor == 2)
-    analogWrite(BR_MOTOR, actSpeed[2]);
-  if (motor == 3)
-    analogWrite(BL_MOTOR, actSpeed[3]);
-}
-
-void runIndividual (int* actSpeed) {
-  analogWrite(FL_MOTOR, actSpeed[0]);
-  analogWrite(FR_MOTOR, actSpeed[1]);
-  analogWrite(BR_MOTOR, actSpeed[2]);
-  analogWrite(BL_MOTOR, actSpeed[3]);
-}
---
+++ src/quad_init.h
@@ -0,0 +1,153 @@
+
+
+#include "Arduino.h"
+// Initial includes
+#include <SoftwareSerial.h>
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#include <PID_v1.h>
+#include <EEPROM.h>
+//#include "MPU6050.h" // not necessary if using MotionApps include file
+
+
+#define ADDR_PITCH_OFFSET 0
+#define ADDR_ROLL_OFFSET 1
+#define FL_MOTOR 3
+#define FR_MOTOR 9
+#define BR_MOTOR 10
+#define BL_MOTOR 11
+
+
+//////////////////////////////////////////////
+//        RemoteXY include library          //
+//////////////////////////////////////////////
+
+// RemoteXY select connection mode and include library 
+//#define REMOTEXY__DEBUGLOGS Serial
+
+// // RemoteXY select connection mode and include library 
+#define REMOTEXY_MODE__BLE_NANO
+
+#include "RemoteXY.h"
+
+// RemoteXY connection settings 
+#define REMOTEXY_SERIAL Serial
+#define REMOTEXY_SERIAL_SPEED 9600
+
+
+// ----------------------------------------------------------------------------
+
+// RemoteXY configurate  
+#pragma pack(push, 1)
+uint8_t RemoteXY_CONF[] =
+  { 255,0,0,4,0,90,0,11,13,0,
+  129,0,47,14,12,2,17,77,111,116,
+  111,114,32,83,112,101,101,100,0,66,
+  130,47,18,13,10,2,26,66,130,67,
+  18,13,10,2,26,129,0,71,14,5,
+  2,17,80,105,116,99,104,0,66,130,
+  47,36,13,10,2,26,129,0,51,32,
+  5,2,17,82,111,108,108,0,66,130,
+  67,36,13,10,2,26,129,0,72,32,
+  4,2,17,89,97,119,0 };
+  
+// this structure defines all the variables and events of your control interface 
+struct {
+
+    // output variables
+  int8_t motor_speed; // =0..100 level position 
+  int8_t show_pitch; // =0..100 level position 
+  int8_t show_roll; // =0..100 level position 
+  int8_t show_yaw; // =0..100 level position 
+
+    // other variable
+  uint8_t connect_flag;  // =1 if wire connected, else =0 
+
+} RemoteXY;
+#pragma pack(pop)
+
+
+/////////////////////////////////////////////
+//           END RemoteXY include          //
+///////////////////////////////////////////// 
+
+
+
+//---------------------------------PID------------------------------------
+//Define Variables we'll be connecting to
+double rollSetpoint, rollInput, rollOutput;
+double pitchSetpoint, pitchInput, pitchOutput;
+double yawSetpoint, yawInput, yawOutput;
+
+//Define the aggressive and conservative Tuning Parameters
+double consKp = 0.5, consKi = 0.05, consKd = 0.05;
+
+PID pitchPID(&rollInput, &rollOutput, &rollSetpoint, consKp, consKi, consKd, DIRECT);
+PID rollPID(&pitchInput, &pitchOutput, &pitchSetpoint, consKp, consKi, consKd, DIRECT);
+//------------------------------------------------------------------------
+
+
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+#include "Wire.h"
+#endif
+
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 mpu;
+//MPU6050 mpu(0x69); // <-- use for AD0 high
+
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+float ypr_cal[3] = {-0.55, -0.03, 0.01}; // initilizes the calibration (done by hand. This should be automated)
+
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+  mpuInterrupt = true;
+}
+
+
+#define ALPHA 0.1
+#define MULTIPLIER 6.67
+float motorBattery;
+
+int targetSpeed[4];
+
+float batt_level = 0;
+
+//Declaration of base functions
+
+float smoothBattery (float prevEntry, float newEntry, float alpha);
+void setSpeed(int val);
+void stabilise (int* currSpeed, int* actSpeed, float rollDiff, float pitchDiff);
+void checkIndividual (int motor, int* actSpeed);
+void runIndividual (int* actSpeed);
+
+void quadInit();
+
+void get_ypr();
+
